b0ccf3fa110e280846b2bed1e0098a4e521ca0ea
SumanDas3001 <sumandas3001@gmail.com>
2019-06-17 11:58:31 +0530

    add profile_pic to authors table and use paperclip gem for image upload

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,4 @@
 class Author < ApplicationRecord
+	has_attached_file :profile_pic, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :profile_pic, content_type: /\Aimage\/.*\z/
 end
