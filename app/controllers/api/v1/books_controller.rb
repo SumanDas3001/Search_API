917dc2efd6018b9b8c0f8e2ac7f163c7375495a0
SumanDas3001 <sumandas3001@gmail.com>
2019-06-18 15:51:51 +0530

    create get_all_data_by_author_name and fetch data form three different table by author name

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -3,7 +3,7 @@ module Api
 		class BooksController < ApplicationController
 
 			### => API Documentation
-			## api :GET, '/api/v1/books', "List of all books."
+			## api :GET, 'http://localhost:3001/api/v1/books?book_name=A Confederacy of Dunces', "List of all books."
 			## param :book_name, String, :required => false, :desc => "Book name, sending the request as url params"
 			
 			### => 'index' API Defination 
@@ -13,7 +13,11 @@ module Api
 				# render json: {status: 'SUCCESS', message:'Loaded books', data:@books},status: :ok
 			end
 
-			def get_all_data_by_book_name
+			## api :GET, 'http://localhost:3001/api/v1/books/get_all_data_by_book_name?name=Pietro Graham', "List of all books."
+			## param :name, String, :required => false, :desc => "Author name, sending the request as url params"
+
+			## => Fetch data from all three table based on author name
+			def get_all_data_by_author_name
 				@all_data = Author.order("created_at DESC")
 				@all_data = @all_data.by_author_name(params[:name]).order("created_at DESC") if params[:name].present?
 			end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace 'v1' do
       resources :books do
         collection do
-          get 'get_all_data_by_book_name'
+          get 'get_all_data_by_author_name'
         end
       end
     end
